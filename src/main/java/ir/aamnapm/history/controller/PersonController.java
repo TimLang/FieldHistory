a3a851b62685785b66a088b8fafec0c5ee92edd7
ali <aliakbari76318@gmail.com>
2020-10-03 08:57:20 +0330

    create person controller

---
+++ src/main/java/ir/aamnapm/history/controller/PersonController.java
@@ -0,0 +1,9 @@
+package ir.aamnapm.history.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/person")
+public class PersonController {
+}
